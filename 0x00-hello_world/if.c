ca9d1de9ca300b591b5d20c7442032f0294848c0
idimmusix <idimmusix@gmail.com>
2022-09-14 06:49:36 +0100

    how far

---
+++ 0x00-hello_world/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int a = 1;
+    int b = 2;
+    if (a>b)
+        return (a);
+    return(0);
+}
