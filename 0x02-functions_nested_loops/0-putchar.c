39cef74b31f8c79ecd97b1f5668ebbf875f6cb40
idimmusix <idimmusix@gmail.com>
2022-09-14 07:40:47 +0100

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[10] = "_putchar\n";
+	int i;
+
+	printf(word);
+
+	return (0);
+}
